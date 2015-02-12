3227c42a19c5b6e3ae1bb54cb243c11d6d52c7ef
Fatimah Zohra <fzohra92@cs.utexas.edu>
2015-02-11 19:12:48 -0600

    Closed #7 #8 Defined methods to get ratings from files

--- Netflix.py
+++ Netflix.py
@@ -6,10 +6,7 @@
 # Keerthana Kumar, Fatimah Zohra
 # -----------------------------
 
-# ---------------
-# global variable
-# ---------------
-current_movie = ""
+import json
 
 # ------------
 # netflix_read
@@ -17,17 +14,44 @@ current_movie = ""
 
 def netflix_read (s) :
     if ":" in s :
-        current_movie = s.split(":")[0]
+        global current_movie
+        current_movie = int(s.split(":")[0])
+        get_movie_avg_rating()
         return -1
     else:
         return s.strip()
 
 # ------------
+# data_collection
+# ------------
+
+def get_movie_avg_rating ():
+    '''
+        Gets the average rating for a single movie
+    '''
+    path = "/u/mck782/netflix-tests/pma459-mvAvgCache.json"
+    json_data=json.loads(open(path).read())
+    global current_movie_rating_avg
+    current_movie_rating_avg = json_data[current_movie]
+
+def get_user_avg_rating (customer_id):
+    '''
+        Gets the average rating of all the movies rated by the user
+        input: customer_id String  
+    '''
+    path = "/u/mck782/netflix-tests/pma459-usrAvgCache.json"
+    json_data=json.loads(open(path).read())
+    return json_data[customer_id]
+
+
+# ------------
 # netflix_eval
 # ------------
 
-def netflix_eval (i, j) :
-    
+def netflix_eval (i) :
+    return 0
+
+
 
 # -------------
 # netflix_print
@@ -51,4 +75,4 @@ def netflix_solve (r, w) :
             rating = netflix_eval(user)
             netflix_print(w, str(rating))
         else:
-            netflix_print(w, current+":")
+            netflix_print(w, str(current_movie)+":")
