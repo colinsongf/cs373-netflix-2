ca2459f2e4617c8045a299e13eabe5789eb9f212
Fatimah Zohra <fzohra92@utexas.edu>
2015-02-19 11:40:04 -0600

    added graphs with best fit lines

--- scripts/plot.py
+++ scripts/plot.py
@@ -5,13 +5,34 @@ plotly.tools.set_credentials_file(username='fzohra92', api_key='69jftnfpjg', str
 
 import plotly.plotly as py
 from plotly.graph_objs import *
+from datetime import datetime
 
-customer_cache_file = "../cs373-netflix/cache.json"
+customer_cache_file = "../caches/cache.json"
 customer_json = json.load(open(customer_cache_file))
+
+
+movie_cache_file = "../caches/moviecache.json"
+movie_cache = json.load(open(movie_cache_file))
+
+#--------------
+#    Parse
+#--------------
+"""
+    a : rating for the movie
+    b : rating date
+
+    x : user count
+    y : user average rating
+
+    x1 : movie period
+    y1 : movie avg
+
+    x2 : movie count
+    y2 : movie avg
+"""
 
 x = []
 y = []
-
 count = 0
 for customer_key, customer_data in customer_json.iteritems():
     if count > 500:
@@ -23,17 +44,38 @@ for customer_key, customer_data in customer_json.iteritems():
         elif(count_key == "average"):
             y.append(count_value)
 
-"""
-    x : count
-    y : average rating
-"""
+x1 = []
+y1 = []
+count = 0
+for m, _ in movie_cache.iteritems():
+    if count < 750:
+        avg = movie_cache[m]["average"]
+        period = movie_cache[m]["period"]
+        x1.append(period)
+        y1.append(avg)
+        count += 1
+    else :
+        break
 
+#--------------
+#    Trace
+#--------------
 trace0 = Scatter(x=x, y=y)
+trace1 = Scatter(x=x1, y=y1)
 
 
+#--------------
+#    Data
+#--------------
 data = Data([trace0])
+data1 = Data([trace1])
 
-layout = Layout(
+
+#--------------
+#    Layout
+#--------------
+
+layout0 = Layout(
     title='Customer Counts x Averages',
     xaxis=XAxis(
         title='count',
@@ -45,7 +87,30 @@ layout = Layout(
         showline='True'
     )
 )
-fig = Figure(data=data, layout=layout)
-plot_url = py.plot(fig, filename='line-style')
 
-#unique_url = py.plot(data, filename = 'basic-line')
+layout1 = Layout(
+    title='Movie Period x Movie Average',
+    xaxis=XAxis(
+        title='Period',
+        showgrid=True,
+        showline=True,
+    ),
+    yaxis=YAxis(
+        title='Average',
+        showgrid=True,
+        showline='True'
+    )
+)
+
+#--------------
+#    Figures
+#--------------
+fig = Figure(data=data, layout=layout0)
+fig1 = Figure(data=data1, layout=layout1)
+
+
+#--------------
+#    Plots
+#--------------
+plot_url = py.plot(fig, filename='customer-count-avg')
+plot_url = py.plot(fig1, filename='movie-period-avg')
